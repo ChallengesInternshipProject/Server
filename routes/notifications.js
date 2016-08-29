302c9dd03692468a68aaac77f10094b07eae5715
Alexander Topalov <ex3m@mail.bg>
2016-08-29 13:40:42 +0300

    Notifications working

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,43 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var Notification = require('../models/notifications');
+var User = require('../models/users');
+var Dare = require('../models/dares')
 var ObjectID = require('mongoose').Types.ObjectId;
 var $q = require('q');
 router.get('/:userID',function(req,res,next){
 	var userID = new ObjectID(req.params.userID);
-	console.log(userID)
+
 	Notification
 	.find({receiver:{$eq:userID}})
 	.sort({createdOn: -1})
-	.populate('sender')
-	.populate('receiver')
-	.exec(function (err, result) {
-		var notifications = result;
-
-		if (err) return next(err);
-		var promise = $q.defer();
-		console.log("LEngth");
-		console.log(notifications.length);
-		for (var i  = 0 ;i < notifications.length; i ++){
-			console.log(notifications[i])
-			switch(notifications[i].refObject){
-				case 'dare':
-					var Dare = require('../models/dares');
-					Dare.findOne({_id:notifications[i].refObjectID}).exec(function(err,dare){
-						notifications[i]["dare"] = dare;
-						notifications[i]['test'] = "sjdij";
-					})
-
-				break;
-				default:
-				break;
-			}
+	.populate([
+		{
+			path : "sender",
+			model : 'User'
+		},
+		{
+			path : 'receiver',
+			model : 'User'
+		},
+		{
+			path : 'refObjectID',
+			model : 'Dare'
 		}
-		promise.resolve(notifications)
-	
-		promise.promise.then(function(finalResult){
-			res.json(finalResult)
-		})
+	])
+	.exec(function (err, result) {
+		res.json(result);
 	});
 })
 
